109c77dd41d0a29c10df10a101b95fd80fea29d9
Giovanni <masteruan@gmail.com>
2019-04-20 22:40:44 +0200

    Update new_listen-for-shutdown.py

--- new_listen-for-shutdown.py
+++ new_listen-for-shutdown.py
@@ -6,13 +6,14 @@ import subprocess
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.wait_for_edge(3, GPIO.FALLING)
-input_state = GPIO.input(3)
-if input_state == False:
-  time.sleep(1)
-  count = count + 1
-else:
-  count = 0
-if count == 5:
-  subprocess.call(['shutdown', '-h', 'now'], shell=False)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while (True):
+  input_state = GPIO.input(18)
+  if input_state == False:
+    time.sleep(1)
+    count = count + 1
+  else:
+    count = 0
+  if count == 5:
+    subprocess.call(['shutdown', '-h', 'now'], shell=False)
